bced3b16ce2b9a2b271334c2607d6cc033960e73
HP <HP@DESKTOP-TK2OJN6>
2023-10-25 00:58:04 +0700

    commit 20

---
+++ src/main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import controller.ManageLoginTBBank;
+
+/**
+ *
+ * @author HP
+ */
+public class Main {
+    public static void main(String[] args) {        
+        String title= "Login Program TB Bank";
+        String[] s= new String[] {"Vietnamese","English","Exit"};
+        new ManageLoginTBBank(title, s).run();       
+    }
+}
